5d3de65ca6734f46f0c907c7b22a9a93ae92ad96
Marina <itsmarinams@gmail.com>
2024-01-13 19:48:24 +0100

    tailwind installed but not working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import './App.css';
+import './output.css';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
-function App() {
-  return (
-    <main>
-      <NavBar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-    </main>
-  );
-}
+// function App() {
+//   return (
+//     <main className="bg-gray-900 body-font text-gray-400">
+//       {/* <NavBar /> */}
+//       {/* <About /> */}
+//       {/* <Projects /> */}
+//       {/* <Skills /> */}
+//       {/* <Contact /> */}
+//       <h1>hola</h1>
+//     </main>
+//   );
+// }
 
-export default App;
+// export default App;
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
