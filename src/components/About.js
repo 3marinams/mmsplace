586c31d8a658f601eb51f75133af9d824e8f9863
Marina <itsmarinams@gmail.com>
2024-01-23 23:55:44 +0100

    About/home: color, img & text updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,27 @@
 import React from "react";
 import '../output.css';
-import geekMarina from '../assets/geekMarina.jpg';
+import geekMarina from '../assets/profile.jpg';
 
 function About() {
     return (
         <section id="about">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black-900">
                         Marina Moreno Soriano
                         {/* <br className="hidden lg:inline-block text-xs" />¿Me encanta construir aplicaciones web.? */}
                     </h1>
-                    <p className="mb-8 leading-relaxed text-xs">
+                    <p className="mb-8 leading-relaxed text-s">
                     UX-UI Manager | Software engineer | VUX-VUI Designer | Visual Designer | Entrepreneur | User researcher expert | Frontend developer
                     </p>
                     {/* <br className="hidden lg:inline-block" /> */}
-                    <p className="mb-8 leading-relaxed text-xl text-white">
-                    I’m a software engineer from Polytechnic University of Catalonia with more than three years of experience in digital products. I’ve worked in Service Design, connecting user and business needs trough feasible digital experiences. 
+                    <p className="mb-8 leading-relaxed text-xl text-black">
+                    My commitment lies in the creation of innovative digital products, bringing extensive experience as a software engineer with a diverse professional background spanning impactful industries. 
                     <br  />
                     <br />
-                    <h4 className="font-bold">I am a digital experience enthusiast.</h4>
+                    <h4 className="font-bold text-blue-900">I am a digital experience enthusiast.</h4>
                     <br className="hidden lg:inline-block" />
-                    <br className="hidden lg:inline-block" />
-                    Nowadays, I am based in Barcelona, but I am really keen on  taking challenging adventures abroad!                       
-                    <br className="hidden lg:inline-block" />
+
                     </p>
                     
                     {/* <div className="flex justify-center">
@@ -39,7 +37,7 @@ function About() {
                         </a>
                     </div> */}
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 ">
                     <img
                         className="object-cover object-center rounded"
                         alt="Marina's emojis"
