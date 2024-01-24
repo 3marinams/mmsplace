5199c12247c9565f11031a8735e9b69a6c3e95d9
Marina <itsmarinams@gmail.com>
2024-01-24 21:01:21 +0100

    Reoder Home order

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <main className="bg-white body-font text-gray-900">
       <NavBar />
       <About />
-      <Projects />
       <Skills />
+      <Projects />
       <Contact />
     </main>
   );
