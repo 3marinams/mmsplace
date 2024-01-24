bdff653cdba6e3b35ed1b44800bb34a5d723fdfe
Marina <itsmarinams@gmail.com>
2024-01-24 18:47:50 +0100

    Text doc + black color in project obj

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import {projects} from "../data";
 
 export default function Projects() {
     return (
-    <section id="projects" className="text-gray-400 bg-white body-font">
+    <section id="projects" className="text-black bg-white body-font">
         {/* <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20"> */}
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
